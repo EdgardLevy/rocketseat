6e1c6220cd2054e532028de8c8ace4f314611dfb
Edgard Levy <edgardlevy.apple@gmail.com>
2019-09-12 16:33:27 -0300

    async/await

--- es6/src/main.js
+++ es6/src/main.js
@@ -1,1 +1,27 @@
-alert("Testando1..2..3");
+//funcionamento assync com promise
+const minhaPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("OK");
+    }, 2000);
+  });
+
+// minhaPromise()
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+
+//usando async/await
+
+async function executaPromise() {
+  //const response = await minhaPromise();
+  //console.log(response);
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+}
+
+executaPromise();
