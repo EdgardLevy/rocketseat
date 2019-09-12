43f94d0270bff030e19d38da62fb9174758560e3
Edgard Levy <edgardlevy.apple@gmail.com>
2019-09-12 16:39:59 -0300

    - configurando o axios

--- es6/src/main.js
+++ es6/src/main.js
@@ -1,27 +1,16 @@
-//funcionamento assync com promise
-const minhaPromise = () =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("OK");
-    }, 2000);
-  });
+import axios from "axios";
 
-// minhaPromise()
-//   .then(response => {
-//     console.log(response);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
-
-//usando async/await
-
-async function executaPromise() {
-  //const response = await minhaPromise();
-  //console.log(response);
-  console.log(await minhaPromise());
-  console.log(await minhaPromise());
-  console.log(await minhaPromise());
+class Api {
+  static async getUserInfo(username) {
+    try {
+      const response = await axios.get(
+        `http://api.github.com/users/${username}`
+      );
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
 
-executaPromise();
+Api.getUserInfo("EdevLevy");
