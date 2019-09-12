28a62e3690be112b787b7a9f87c01957a8b4cd1a
Edgard Levy <edgardlevy.apple@gmail.com>
2019-09-12 14:23:34 -0300

    operacoes em array

--- es6/bundle.js
+++ es6/bundle.js
@@ -1,12 +1,24 @@
 "use strict";
 
-//escopo de variaveis
-function teste(x) {
-  //variaveis decladas dentro da funcao
-  var y = 2;
-  if (x > 5) console.log(x, y);
-} //fora de escopo
+var arr = [1, 3, 4, 5, 8, 9]; //map percorre o vetor e retorna alguma info em outro vetor
 
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log(newArr); //reduce percorre o vetor para 'reduzir' em uma unica informacao
 
-console.log(y);
-teste(10);
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum);
+var filter = arr.filter(function (item) {
+  //verifica se o numero eh par
+  //utilizando o operador de modulo que retorna o diferencial da divisao
+  return item % 2 === 0;
+});
+console.log(filter); //find busca alguma coisa no vetor
+
+var find = arr.find(function (item) {
+  return item === 4;
+});
+console.log(find);
